a23f6b001687f4e7fc1c3204dcc4fda79f19bd8e
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-17 22:29:17 +0200

    Added tokio and futures.

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,15 @@
 
 //! Server and client SSH library, based on *ring* for its crypto, and
 //! tokio/futures for its network management.
+
+#[macro_use]
+extern crate tokio_core;
+extern crate tokio_io;
+#[macro_use]
+extern crate futures;
+
+#[derive(Clone, Copy)]
+enum Status {
+    Ok,
+    Disconnect,
+}
